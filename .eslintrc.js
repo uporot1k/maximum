1ed35fd566088fe241c2dc0172079d1f0871e867
Egor <uporot1k@yandex.ru>
2020-09-29 14:37:39 +0300

    .

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,6 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'import/prefer-default-export': 'off',
-    'max-len': 'off'
+    'max-len': 'off',
   },
 };
